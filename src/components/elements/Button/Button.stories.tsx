9dacf207e8b441751c18b758267adaca2b7653c5
raprayoga <prayogarafie@gmail.com>
2023-09-26 16:55:41 +0700

    feat(button): create element button w/ stories & test

---
+++ src/components/elements/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Button from './index'
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  args: {
+    children: 'Button',
+    className: 'w-36',
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Default: Story = {}
